0914ee515e70321a8073b4375190d37352452fd7
Wouter Bouvy <github@wbouvy.com>
2018-04-23 12:52:24 +0200

    clean up java example

--- java/run.sh
+++ java/run.sh
@@ -2,15 +2,17 @@
 
 set -e
 
+function finish {
+  echo "Killing mock server..."
+  docker rm -f java-example-server
+}
+trap finish EXIT
+
+./gradlew build
+
 echo "Starting mock server..."
 docker run -id -p9090:9090 --name java-example-server hiberglobal/mock-server
 echo "Mock server running on port 9090"
 
-./gradlew build
-
 echo "Doing client calls..."
 ./gradlew example:run
-
-echo "Killing mock server..." 
-docker rm -f java-example-server
-
