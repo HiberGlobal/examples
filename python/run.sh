771af4c67aef3ada196624230e064b111c84868d
Wouter Bouvy <github@wbouvy.com>
2018-04-23 12:50:22 +0200

    minor fixes for the python example

--- python/run.sh
+++ python/run.sh
@@ -2,18 +2,21 @@
 
 set -e
 
+function finish {
+  echo "Killing mock server..."
+  docker rm -f python-example-server
+}
+trap finish EXIT
+
+pip install -r requirements.txt
+mkdir -p generated
+python -m grpc_tools.protoc -I../hiber-api --python_out=generated --grpc_python_out=generated ../hiber-api/*.proto
+
 echo "Starting mock server..."
 docker run -id -p9090:9090 --name python-example-server hiberglobal/mock-server
 echo "Mock server running on port 9090"
 
-pip install -r requirements.txt
-
-mkdir -p generated
-python -m grpc_tools.protoc -I../hiber-api --python_out=generated --grpc_python_out=generated ../hiber-api/*.proto
+exit 1;
 
 echo "Doing client calls..."
 python client.py
-
-echo "Killing mock server..." 
-docker rm -f python-example-server
-
