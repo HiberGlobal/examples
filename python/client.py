2c623c4bf9d6439b769a74fba329e02074586a71
Wouter Bouvy <github@wbouvy.com>
2018-04-23 19:30:45 +0200

    add comments about http(s)

--- python/client.py
+++ python/client.py
@@ -7,6 +7,7 @@ import generated.modem_pb2_grpc
 
 
 def run():
+    # the mock server is http only, use https when connecting to https://dev.hiber.global
     channel = grpc.insecure_channel('localhost:9090')
     metadata = [('authorization', 'bearer my-super-secret-token')]
     currentUserStub = generated.currentuser_pb2_grpc.CurrentUserServiceStub(channel)
--- ruby/client.rb
+++ ruby/client.rb
@@ -7,10 +7,12 @@ require 'modem_services_pb'
 def main
   metadata = {metadata: {'authorization': 'bearer my-super-secret-token'}}
 
+  # the mock server is http only, use https when connecting to https://dev.hiber.global
   currentUserStub = Hiber::User::CurrentUserService::Stub.new('localhost:9090', :this_channel_is_insecure)
   email = currentUserStub.current_user(Hiber::User::CurrentUserRequest.new(), metadata).email
   p "Current iuser is: #{email}"
 
+  # the mock server is http only, use https when connecting to https://dev.hiber.global
   modemStub = Hiber::Modem::ModemService::Stub.new('localhost:9090', :this_channel_is_insecure)
   modems = modemStub.list(Hiber::Modem::ListModemsRequest.new(), metadata).modems
   p "Modems: #{modems}"
