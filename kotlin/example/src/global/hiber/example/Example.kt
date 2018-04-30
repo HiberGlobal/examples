521e74f4a8eb63730aceddc3ad56fa6ade8cf705
Wouter Bouvy <github@wbouvy.com>
2018-04-30 17:21:36 +0200

    kotlin example code for api.dev.hiber.global

--- kotlin/example/src/global/hiber/example/Example.kt
+++ kotlin/example/src/global/hiber/example/Example.kt
@@ -7,14 +7,16 @@ import io.grpc.ManagedChannelBuilder
 
 class Example(val host: String, val port: Int, val token: String) {
   private val channel: ManagedChannel = ManagedChannelBuilder.forAddress(host, port)
-    .usePlaintext(true) // the mock server is http only, use https when connecting to https://dev.hiber.global
+//    .useTransportSecurity()
+    .usePlaintext()
     .build()
   val currentUserServiceClient = CurrentUserServiceClient(channel, token)
   val modemServiceClient = ModemServiceClient(channel, token)
 }
 
 fun main(args: Array<String>) {
-  val client = Example("localhost", 9090, "my-super-secret-token")
+//  val client = Example("api.dev.hiber.global", 443, "YOUR-HIBER-DEV-TOKEN-HERE") // with useTransportSecurity
+  val client = Example("localhost", 9090, "my-super-secret-token") // use plaintext
 
   val user = client.currentUserServiceClient.currentUser()
   System.out.println("Current user: $user")
