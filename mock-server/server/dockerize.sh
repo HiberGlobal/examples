dfee0d0807f6d4ce1314b28c73e6a048945902ed
Wouter Bouvy <github@wbouvy.com>
2018-08-21 11:02:02 +0200

    update dockerize script

--- mock-server/server/dockerize.sh
+++ mock-server/server/dockerize.sh
@@ -2,18 +2,21 @@
 
 set -e
 
-usage() { echo "Use -i to specify the tag"; }
+usage() { echo "Use -i to specify the tag (defaults to hiberglobal/mock-server), -v to specify the version"; }
 
 IMAGE="hiberglobal/mock-server"
 
-while getopts ':hi:' option; do
+while getopts ':hi:v:' option; do
   case ${option} in
     h  ) usage; exit;;
+    v  ) VERSION=$OPTARG;;
     i  ) IMAGE=$OPTARG;;
     \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
     :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
   esac
 done
 
+if [[ "$VERSION" == "" ]]; then usage; exit 1; fi
+
 ./gradlew build -PdockerizeServer
-docker build -t ${IMAGE} server
+docker build -t ${IMAGE}:$VERSION -t ${IMAGE}:latest server
