3011d7d84ca0549adf2b10d4288f616dd3784246
Wouter Bouvy <github@wbouvy.com>
2019-05-16 15:52:27 +0200

    correct the port

--- java-webhook-reciever/test.sh
+++ java-webhook-reciever/test.sh
@@ -17,6 +17,6 @@ echo "Server starting at process $!"
 
 sleep 2
 
-curl -vX POST http://localhost:8080 --data-binary @test.json \
+curl -vX POST http://localhost:8070 --data-binary @test.json \
 --header "X-Hub-Signature: B106EE4C741E7688ADB713402C16921BE50E55DA8762D2257377E4277D4CE6C1" \
 --header "Content-Type: application/json"
